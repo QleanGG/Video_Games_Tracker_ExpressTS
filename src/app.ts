83a61aa1b597cb232a5a605dbd8ae6d79f85c8b2
QleanGG <guyguz1@gmail.com>
2024-05-11 20:02:50 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, {Request, Response} from "express";
+// import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req:Request, res:Response, next) => {
+    res.send('Hello World!')
+});
+
+
+
+app.listen(PORT,() => {
+    console.log(`Server is running on http://localhost:${PORT}`);   
+})
+
