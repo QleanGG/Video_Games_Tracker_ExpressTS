72ed4c9b0800ada4c0f63e9d0427a1ec78612678
QleanGG <guyguz1@gmail.com>
2024-05-21 00:38:20 +0300

    clean profilecontroller after middleware implamentation

--- src/controllers/ProfileController.ts
+++ src/controllers/ProfileController.ts
@@ -1,49 +1,40 @@
-import { Request,Response } from "express";
-import {ProfileService } from "../services/ProfileService";
-import { ensureAuthenticated } from '../utils/authUtils';
+import { Request, Response } from "express";
+import { ProfileService } from "../services/ProfileService";
+import { ensureAuthenticated } from "../utils/authUtils";
 
 class ProfileController {
-    private profileService: ProfileService;
+	private profileService: ProfileService;
 
-    constructor() {
-        this.profileService = new ProfileService();
-    }
+	constructor() {
+		this.profileService = new ProfileService();
+	}
 
-    async getProfile(req: Request, res: Response): Promise<void> {
-        if (!ensureAuthenticated(req, res)) {
-            return;
-        }
-        try {
-            const userId = (req.user as any).id;
-            const result = await this.profileService.getProfile(userId);
-            if ('message' in result) {
-                res.status(404).json(result);
-            } else {
-                res.status(200).json(result);
-            }
-        } catch (error) {
-            res.status(500).json({message: (error as Error).message});
-        }
-    }
+	async getProfile(req: Request, res: Response): Promise<void> {
+		if (!ensureAuthenticated(req, res)) {
+			return;
+		}
+		const userId = (req.user as any).id;
+		const result = await this.profileService.getProfile(userId);
+		if ("message" in result) {
+			res.status(404).json(result);
+		} else {
+			res.status(200).json(result);
+		}
+	}
 
-    async updateProfile(req: Request, res:Response): Promise<void> {
-        if (!ensureAuthenticated(req, res)) {
-            return;
-        }
-        
-        try {
-            const userId = (req.user as any).id;
-            const profileData = req.body;
-            const result = await this.profileService.updateProfile(userId, profileData);
-            if ('message' in result) {
-                res.status(404).json(result);
-            } else {
-                res.status(200).json(result);
-            }
-        } catch (error) {
-            res.status(500).json({message: (error as Error).message});
-        }
-    }
+	async updateProfile(req: Request, res: Response): Promise<void> {
+		if (!ensureAuthenticated(req, res)) {
+			return;
+		}
+		const userId = (req.user as any).id;
+		const profileData = req.body;
+		const result = await this.profileService.updateProfile(userId, profileData);
+		if ("message" in result) {
+			res.status(404).json(result);
+		} else {
+			res.status(200).json(result);
+		}
+	}
 }
 
 export default ProfileController;
