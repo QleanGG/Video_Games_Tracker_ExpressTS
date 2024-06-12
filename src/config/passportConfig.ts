f47668d4bcae1653a0a092b26ecaccbfe1a85003
QleanGG <guyguz1@gmail.com>
2024-06-12 08:03:18 +0300

    add deploy fix for passportConfig

--- src/config/passportConfig.ts
+++ src/config/passportConfig.ts
@@ -1,3 +1,4 @@
+import './envConfig';
 import passport from "passport";
 import { Strategy as LocalStradegy } from "passport-local";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
