c2579e8b254492411fe9edda81245a529ec446e8
QleanGG <guyguz1@gmail.com>
2024-06-13 17:27:39 +0300

    check env vars

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,17 @@ import app from "./app";
 import dotenv from "dotenv";
 
 dotenv.config();
+const SECRET_KEY = process.env.SECRET_KEY || 'defaultSecretKey';
+const REDIS_URL = process.env.UPSTASH_REDIS_REST_URL || 'redis://localhost:6379';
+const REDIS_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3001';
 
 // Starting db and port
 initializeDatabase().then(() => {
     const PORT = process.env.SERVER_PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server running on ${PORT}`);
+        console.log(REDIS_URL,"frontend url: ", FRONTEND_URL )
     });
 }).catch(error => {
     console.error('Failed to start due to database initialization errors:', error);
