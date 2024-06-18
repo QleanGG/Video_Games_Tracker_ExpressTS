40b8c5f33b2c0c02c71ea7759957bdc5acfbcd17
QleanGG <guyguz1@gmail.com>
2024-06-18 15:29:57 +0300

    reverting server changes

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,16 @@
 import { initializeDatabase } from "./database/database";
 import app from "./app";
 import dotenv from "dotenv";
-import redisClient from "./config/redisClient";
 
 dotenv.config();
 
-const startServer = async () => {
-	try {
-		await initializeDatabase();
-		const PORT = process.env.SERVER_PORT || 3000;
-
-		// Ensure Redis client is ready before starting the server
-		redisClient.on("ready", () => {
-			app.listen(PORT, () => {
-				console.log(`Server running on ${PORT}`);
-				console.log(`We are on ${process.env.NODE_ENV}`);
-			});
-		});
-
-		redisClient.on("error", (err) => {
-			console.error("Redis client error:", err);
-			console.log("Starting server without Redis...");
-			app.listen(PORT, () => {
-				console.log(`Server running on ${PORT}`);
-				console.log(`We are on ${process.env.NODE_ENV}`);
-			});
-		});
-	} catch (error) {
-		console.error("Failed to start due to database initialization errors:", error);
-	}
-};
-
-startServer();
+// Starting db and port
+initializeDatabase().then(() => {
+    const PORT = process.env.SERVER_PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+        console.log(`we are on ${process.env.NODE_ENV}`)
+    });
+}).catch(error => {
+    console.error('Failed to start due to database initialization errors:', error);
+});
